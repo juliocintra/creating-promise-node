277c571d2827f2eef461006643521401999eb951
Julio Cesar <julinn_cesar@hotmail.com>
2019-10-21 22:15:39 -0300

    finished making promises

--- new_promise.js
+++ new_promise.js
@@ -1,3 +1,7 @@
+'use strict';
+
+// O poder das promises é poder encadear requisições assincronas
+
 const STATE = {
     PENDING: 'pending',
     FULFILLED: 'fulfilled',
@@ -14,8 +18,8 @@ class NPRomise {
         this.value = undefined;
         this.onFulFillChain = [];
         this.onRejectCallChain = [];
-        
-        executor(this.resolve.bind(this));
+
+        executor(this.resolve.bind(this), this.reject.bind(this));
     }
 
     then(onFulFill) {
@@ -33,16 +37,54 @@ class NPRomise {
         });
     }
 
+    catch(onReject) {
+        return new NPRomise((resolve, reject) => {
+            const onRejected = (res) => {
+                try {
+                    resolve(onReject(res));
+                } catch (error) {
+                    reject(error);
+                }
+            }
+
+            if (this.state === STATE.REJECTED) {
+                onRejected(this.value);
+            } else {
+                this.onRejectCallChain.push(onRejected);
+            }
+        });
+    }
+
     resolve(res) {
         if (this.state !== STATE.PENDING) {
             return;
         }
 
+        if (res != null && typeof res.then == 'function') {
+            return res.then(this.resolve.bind(this));
+            // Como o retorno "res" de outra promise está sendo uma promise,
+            // o "then" dela precisa ser resolvido
+        }
+
         this.state = STATE.FULFILLED;
         this.value = res;
 
-        for(const onFulfilled of this.onFulFillChain) {
+        for (const onFulfilled of this.onFulFillChain) {
             onFulfilled(res);
+        }
+    }
+
+    reject(error) {
+
+        if (this.state !== STATE.PENDING) {
+            return;
+        }
+
+        this.state = STATE.REJECTED;
+        this.value = error;
+
+        for (const onRejected of this.onRejectCallChain) {
+            onRejected(error);
         }
     }
 }
